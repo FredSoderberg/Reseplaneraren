7a248d711b7fdc25b2a705db89b3823b292e5be0
talthin <tom.althin@gmail.com>
2015-10-13 13:34:48 +0200

    arg main implenment

--- Resan/main.c
+++ Resan/main.c
@@ -12,8 +12,59 @@ void readline(char buffer[], size_t n, FILE *stream)
     *strchr(buffer, '\n') = '\0';
 }
 
-int main(int argc, char const *argv[])
+void calc_travel(char *from, char *to, char *start)
 {
+  printf("nice job mistah");
+}
+
+int main(int argc, char *argv[])
+{
+  
+  if(argc > 1)
+    {
+      char *from;
+      char *to;
+      char *start;
+      char *arg;
+      
+      
+      for(int i = 1; i < argc; i++) //behöver inte veta filnamnet därför i = 1
+	{
+	  arg = argv[i];
+	  
+	  if(arg[0] == '-' && arg [1] == '-')
+	    {
+	      char *value = argv[++i];
+	      
+	      arg += 2;
+	      
+	      if(strcmp(arg, "from") == 0)
+		{
+		  from = strdup(value);
+		}
+	      
+	      else if(strcmp(arg, "to") == 0)
+		{
+		  to = strdup(value);
+		}
+	      else if(strcmp(arg, "start") == 0)
+		{
+		  start = strdup(value);
+		}
+	      else 
+		{
+		  printf("wrong input");
+		  break;
+		}
+	    }
+	}
+      if(from && start && to)
+	{
+	  calc_travel(from, to, start);
+	}
+    }
+  else {
+    
     network_t *n = network_parse(fopen("data.txt", "r"));
     char from[BUFFER_SIZE];
     char to[BUFFER_SIZE];
@@ -22,20 +73,24 @@ int main(int argc, char const *argv[])
     puts("================================");
     puts("");
     while(true)
-        {
-            puts("What is your starting station?");
-            readline(from, BUFFER_SIZE, stdin);
-            if(from[0] == '\0')
-                {
-                    break;
-                }
-            puts("Where would you like to go?");
-            readline(to, BUFFER_SIZE, stdin);
-            list_t *travels = network_find_travels(n, from, to);
-            list_foreach(travels, (void_fun_t)travel_print);
-	    list_foreach(travels,free);
-        }
+      {
+	puts("What is your starting station?");
+	readline(from, BUFFER_SIZE, stdin);
+	if(from[0] == '\0')
+	  {
+	    break;
+	  }
+	puts("Where would you like to go?");
+	readline(to, BUFFER_SIZE, stdin);
+	list_t *travels = network_find_travels(n, from, to);
+	list_foreach(travels, (void_fun_t)travel_print);
+	list_foreach(travels,free);
+      }
     puts("Good bye!");
-//  network_print(n);
+    //  network_print(n);
     return 0;
+  }
+  return 0;
 }
+
+
